9053d3da8bbfba588b51d546d0adc3e23f4839a7
jin-takahashi09 <takahashijin2009@gmail.com>
2026-08-30 16:47:00 +0900

    hero画像のpreloadを追加

---
+++ src/lib/hero-image.ts
@@ -0,0 +1,5 @@
+import heroImg from '../assets/img/hero.jpg';
+
+export const heroImage = heroImg;
+export const heroWidths = [390, 480, 640, 768, 800, 960, 1280, 1477] as const;
+export const heroSizes = '100vw';
